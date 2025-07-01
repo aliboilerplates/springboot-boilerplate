82c3aa1d693734736bd2005626e6b92747db78e9
zinlabdev <zinlabdev@gmail.com>
2025-07-01 05:22:41 -0700

    feat: add flyway for database migrations

--- src/main/resources/db/migration/V1_0_0__create_tutorial_table.sql
+++ src/main/resources/db/migration/V1_0_0__create_tutorial_table.sql
@@ -1,9 +1,9 @@
 
 -- Tutorials Table
-
 CREATE TABLE IF NOT EXISTS tutorial (
     "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     "title" TEXT NOT NULL UNIQUE,
     "description" TEXT,
-    "published" BOOLEAN NOT NULL DEFAULT FALSE
+    "published" BOOLEAN NOT NULL DEFAULT FALSE,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
